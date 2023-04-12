73feee0b8678cf5aa5790f85bbb89b5cf71bd815
EleahBurman <eleahjburman@gmail.com>
2023-04-12 17:30:01 -0400

    Adding paths to To-Dos

--- controllers/skills.js
+++ controllers/skills.js
@@ -14,6 +14,40 @@ function index(req, res) {
   })
 }
 
+function newSkills(req, res) {
+  res.render('skills/new')
+}
+
+function create(req, res) {
+  console.log(req.body)
+  req.body.done = false
+  Todo.create(req.body)
+  .then(todo => {
+		// Notice we are doing a redirect here!
+    res.redirect('/todos')
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/todos')
+  })
+}
+
+function show(req, res) {
+  Todo.findById(req.params.todoId)
+  .then(todo => {
+    res.render('todos/show', {
+      todo: todo
+    })
+  })
+  .catch(error => {
+    console.log(error)
+    res.redirect('/todos')
+  })
+}
+
 export {
-  index
+  index,
+  newSkills as new,
+  create,
+  show
 }
--- routes/skills.js
+++ routes/skills.js
@@ -1,1 +1,14 @@
-import {skills} from '../data/skill-data'
+import { Router } from 'express'
+
+import * as skillsCtrl from '../controllers/skills.js'
+
+const router = Router()
+// GET localhost:3000/todos
+router.get('/', skillsCtrl.index)
+// GET localhost:3000/todos/new
+router.get('/new', skillsCtrl.new)
+// POST localhost:3000/todos
+router.get('/:todoId', skillsCtrl.show)
+router.post('/', skillsCtrl.create)
+
+export {router}
